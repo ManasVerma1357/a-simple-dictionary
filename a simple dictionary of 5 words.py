77dac5aebbd5d92a5cebee9554f2b3a71020cce1
manas verma <71920045+ManasVerma1357@users.noreply.github.com>
2020-09-26 13:08:35 +0530

    Add files via upload

---
+++ a simple dictionary of 5 words.py
@@ -0,0 +1,14 @@
+''' 23.9.20
+author - manas verma
+a simple dictionary'''
+print("welcome to my dictionary")
+print("sorry,you can just find the meaning of mutable,immutable,abandoned and machine")
+d1 = {"mutable":"can change", "immutable": "cannot change", "abandoned":"having been deserted or left",
+              "machine":"a thing that makes our life easy"}
+print("pls search your meaning of the word")
+search = input()
+print("the meaning of the word is")
+print(d1[search])
+print("thanks for using my devloping program.")
+
+
